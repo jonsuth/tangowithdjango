574f7ac6229ae0dc2dc725c2ec79c67da67734ac
Jon Suthakaran <jonathan.suthakaran@gmail.com>
2016-01-25 16:50:04 +0000

    Updated model and view

--- tangowithdjango_project/rango/views.py
+++ tangowithdjango_project/rango/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Rango says Hello world! <br/> <a href=\'/rango/about\'>About</a>')
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):
-    return HttpResponse('Rango says here is the about page <br/> <a href=\'/rango/\'>Index</a>')
+    context_dict ={}
+    return render(request, 'rango/about.html', context_dict)
