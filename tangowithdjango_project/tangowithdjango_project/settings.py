e3828b28d0e46fa3293dac48aca9bb88378d0b9c
Jon Suthakaran <jonathan.suthakaran@gmail.com>
2016-01-25 16:51:41 +0000

    Updated settings and urls

--- tangowithdjango_project/tangowithdjango_project/settings.py
+++ tangowithdjango_project/tangowithdjango_project/settings.py
@@ -12,7 +12,19 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
+TEMPLATE_DIRS = ('H:/Workspace/WAD2/tangowithdjango/tangowithdjango_project', TEMPLATE_PATH, )
+
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (STATIC_PATH, )
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -81,4 +93,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+
+
