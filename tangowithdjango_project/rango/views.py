670b3dfc1ad6d5b995c9af446145be907995dfc3
Jon Suthakaran <jonathan.suthakaran@gmail.com>
2016-02-01 13:56:18 +0000

    Added url slugs

--- tangowithdjango_project/rango/views.py
+++ tangowithdjango_project/rango/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category
+from rango.models import Page
+
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     return render(request, 'rango/index.html', context_dict)
 
@@ -12,3 +15,22 @@ def index(request):
 def about(request):
     context_dict ={}
     return render(request, 'rango/about.html', context_dict)
+
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        # Try search for the slug in the model. If we can't find it the methods raise an DoesNotExist exception.
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+
+        pages = Page.objects.filter(category=category)  # Retrieve all the associated pages.
+        context_dict['pages'] = pages  # Add result to the dictionary.
+        context_dict['category'] = category  # Also add the category object to the dictionary.
+
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
+
