07d5fd9b1e1ac30628388070921634ec46e8f9de
Jon Suthakaran <jonathan.suthakaran@gmail.com>
2016-02-08 14:28:44 +0000

    Chapter 8 done

--- tangowithdjango_project/rango/views.py
+++ tangowithdjango_project/rango/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 
 def index(request):
@@ -28,9 +30,56 @@ def category(request, category_name_slug):
         pages = Page.objects.filter(category=category)  # Retrieve all the associated pages.
         context_dict['pages'] = pages  # Add result to the dictionary.
         context_dict['category'] = category  # Also add the category object to the dictionary.
+        context_dict['category_name_slug'] = category_name_slug
 
     except Category.DoesNotExist:
         pass
 
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)  # Save form if we are provided with valid data
+
+            return index(request)
+        else:
+            print form.errors
+    else:
+        form = CategoryForm()
+
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.views = 0
+                page.save()
+                # probably better to use a redirect here.
+                return category(request, category_name_slug)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    context_dict = {'form': form, 'category': cat}
+
+    return render(request, 'rango/add_page.html', context_dict)
+
+
 
